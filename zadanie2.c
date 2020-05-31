e19a6e41283ae3ffbc4c982f178d60be4932302a
michal1domanski <michal.domanski22@gmail.com>
2020-05-31 16:08:50 +0200

    heap working but serve is dumb

--- zadanie2.c
+++ zadanie2.c
@@ -12,28 +12,6 @@ RT_HEAP stos;
 RT_SEM sem;
 int licznik;
 
-void fromheap(void *arg) {
-	printf("czekam na sterte\n");
-	RT_HEAP heap;
-	rt_heap_bind(&heap, "myheap", TM_INFINITE);
-
-	printf("czekam na mutex\n");
-	RT_MUTEX mutex;
-	rt_mutex_bind(&mutex, "mymutex", TM_INFINITE);
-	
-	char *ptr;
-	char lastmod = 255;
-	rt_heap_alloc(&heap, 0, TM_INFINITE, (void**)&ptr);
-	while(1){
-		rt_mutex_acquire(&mutex, TM_INFINITE);
-		if (ptr[2] != 1) {
-			printf("%hi, %hi\n", ptr[0], ptr[1]);
-		}
-		rt_mutex_release(&mutex);
-		rt_task_sleep(10000000000LL);
-	}
-}
-
 void serve(void *arg) {
 	RT_TASK_INFO info;
 	rt_task_inquire(rt_task_self(), &info);
@@ -53,6 +31,36 @@ void serve(void *arg) {
 		printf("%s zwolnilo semafor.\n", info.name);
 	}
 }
+
+
+void fromheap(void *arg) {
+	printf("czekam na sterte\n");
+	RT_HEAP heap;
+	rt_heap_bind(&heap, "myheap", TM_INFINITE);
+
+	printf("czekam na mutex\n");
+	RT_MUTEX mutex;
+	rt_mutex_bind(&mutex, "mymutex", TM_INFINITE);
+	
+	char *ptr;
+	char lastmod = 255;
+	rt_heap_alloc(&heap, 0, TM_INFINITE, (void**)&ptr);
+	while(1){
+		rt_mutex_acquire(&mutex, TM_INFINITE);
+		if (ptr[0] != lastmod) {
+			RT_TASK zadanko;
+			rt_task_create(&zadanko, "cykdwojeczka", 0, ptr[1], 0);
+			rt_task_set_periodic(&zadanko, TM_NOW, TM_NOW+ptr[0]);
+			rt_task_start(&zadanko, &serve, NULL);
+			printf("%hi, %hi\n", ptr[0], ptr[1]);
+			ptr[0] = lastmod;
+		}
+		rt_mutex_release(&mutex);
+		rt_task_sleep(10000000000LL);
+	}
+
+}
+
 
 int main(int a, char** b) {
 	mlockall(MCL_CURRENT | MCL_FUTURE);
