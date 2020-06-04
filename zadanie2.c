e88931c9d162300d06390b99795c1a2988dd6a15
michal1domanski <michal.domanski22@gmail.com>
2020-06-04 19:46:25 +0200

    zad 2 working

--- zadanie2.c
+++ zadanie2.c
@@ -6,92 +6,64 @@
 #include <alchemy/sem.h>
 #include <alchemy/heap.h>
 #include <alchemy/mutex.h>
+#include <stdlib.h>
 
-RT_TASK zadania[3];
-RT_TASK task;
-RT_HEAP stos;
-RT_SEM sem;
-int licznik;
+const int TASK_MAX = 55;
 
-void serve(void *arg) {
-	RT_TASK_INFO info;
-	rt_task_inquire(rt_task_self(), &info);
-
-	printf("%s utworzone. \n", info.name);
-
-	while(1) {
-		printf("%s czeka na semafor.\n", info.name);
-		rt_sem_p(&sem, TM_INFINITE);
-		printf("%S otrzymalo semafor.\n", info.name);
-
-		printf("%s ustawia licznik na %i.\n", info.name, ++licznik);
-
-		rt_task_wait_period(NULL);
-
-		rt_sem_v(&sem);
-		printf("%s zwolnilo semafor.\n", info.name);
-	}
-}
-
-int globalna = 0;
+typedef union{
+	struct{
+		uint64_t prio, time;
+	};
+	long long ind;
+}task_data;
 
 void fun(void *arg) {
-	globalna = globalna + 1;
-	printf("globalna = %d\n", globalna);
-	rt_timer_spin((*((long long*)(arg))));
+	rt_task_set_mode(0, T_LOCK, NULL);
+	RT_TASK_INFO info;
+	rt_task_inquire(rt_task_self(), &info);
+	printf("rozpoczecie zadania %s: %llu\n", info.name, rt_timer_read());
+	rt_timer_spin((*((uint64_t*)(arg))));
+	printf("zakonczenie zadania %s: %llu\n", info.name, rt_timer_read());
+}
+
+void fromheap(void *arg){
+	printf("czekam na sterte\n");
+	RT_HEAP heap;
+	rt_heap_create(&heap, "myheap", TASK_MAX*sizeof(task_data), H_SINGLE | H_PRIO);
+	printf("czekam na mutex\n");
+	RT_MUTEX mutex;
+	rt_mutex_create(&mutex, "mymutex");
+	printf("czekam na semafor\n");
+	RT_SEM sem;
+	rt_sem_create(&sem, "mysem", 0, 0);
+	task_data* ptr;
+	rt_heap_alloc(&heap, 0, TM_INFINITE, (void**)&ptr);
+	RT_TASK task[TASK_MAX];
+	int zadanie = 0;
+	long long ind = 0;
+
+	while(1){
+		rt_sem_p(&sem, TM_INFINITE);
+		rt_mutex_acquire(&mutex, TM_INFINITE);
+		ind = (ind + 1) % TASK_MAX;
+		uint64_t *time = malloc(sizeof(uint64_t));
+		*time = ptr[ind].time;
+		printf("%llu\n", ptr[ind].time);
+		char name[12];
+		sprintf(name, "zadanie %d", zadanie);
+		printf("%s\n", &ptr[ind].time);
+		rt_task_create(&task[zadanie], name, 0, ptr[ind].prio, 0);
+		rt_task_start(&task[zadanie], &fun, (void*)time);
+		rt_mutex_release(&mutex);
+		zadanie = (zadanie + 1) % TASK_MAX;
+	}
 }
 
 int main(int a, char** b) {
 	mlockall(MCL_CURRENT | MCL_FUTURE);
-	printf("czekam na sterte\n");
-	RT_HEAP heap;
-	rt_heap_bind(&heap, "myheap", TM_INFINITE);
-
-	printf("czekam na mutex\n");
-	RT_MUTEX mutex;
-	rt_mutex_bind(&mutex, "mymutex", TM_INFINITE);
-
-	uint64_t* ptr;
-	char lastmod = 0;
-	rt_heap_alloc(&heap, 0, TM_INFINITE, (void**)&ptr);
-	SRTIME start;
-	SRTIME end;
-	int i = 1;
-	while(1){
-		rt_mutex_acquire(&mutex, TM_INFINITE);
-		if (ptr[0] != lastmod) {
-			start = rt_timer_ticks2ns(rt_timer_read());
-			printf("rozpoczecie zadania %d = %llu\n", i, start);
-			RT_TASK task;
-			rt_task_create(&task, "zadanie", 0, ptr[1], 0);
-			rt_task_set_periodic(&task, TM_NOW, TM_NOW+ptr[0]);
-			rt_task_start(&task, &fun, &ptr[0]);
-			printf("czas: %llu, priorytet: %d\n", ptr[0], ptr[1]);
-			ptr[0] = lastmod;
-			end = rt_timer_ticks2ns(rt_timer_read());
-			printf("zakonczenie zadania %d = %llu\n", i, end);
-			i = i + 1;
-		}
-		rt_mutex_release(&mutex);
-		rt_task_sleep(1000000000LL);
-	}
-
-	/*char name[3][10];
-	sprintf(name[0], "zad 1");
-	sprintf(name[1], "zad 2");
-	sprintf(name[2], "zad 3");
-	for(int i=0;i<3;i++){
-		char name[10];
-		snprintf(name, 5, "zad %d",i);
-		rt_task_create(&zadania[i], name[i], 0, 10, 0);
-		rt_task_set_periodic(&zadania[i], TM_NOW, 5000000000LL);
-		rt_task_start(&zadania[i], &fromheap, 0);
-	}*/
-
+	RT_TASK task;
+	rt_task_create(&task, "fromtask", 0, 10, 0);
+	rt_task_start(&task, &fromheap, NULL);
 	pause();
 }
-
-
-
-
 
--- zadanie2XD.c
+++ zadanie2XD.c
@@ -3,50 +3,47 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <alchemy/heap.h>
-#include <alchemy/heap.h>
+#include <alchemy/sem.h>
 #include <alchemy/mutex.h>
 
-void toheap(void *arg) {
-	RT_HEAP heap;
+const int TASK_MAX = 55;
 
-	rt_heap_create(&heap, "myheap", 100, H_SINGLE | H_PRIO);
+typedef union{
+	struct{
+		uint64_t prio, time;
+	};
+	long long ind;
+}task_data;
 
-	RT_MUTEX mutex;
-	rt_mutex_create(&mutex, "mymutex");
-
-	uint64_t* ptr;
-	rt_heap_alloc(&heap, 0, TM_INFINITE, (void**)&ptr);
-
-	uint64_t **arr;
-	arr = (uint64_t**) malloc(sizeof(uint64_t*));
-	arr[0] = (uint64_t*) malloc(2*sizeof(uint64_t));
-	char name[100] = "stosik";
-	int i = 0;
-	while(1) {
-		printf("podaj czas trwania zadania\n");
-		scanf("%llu", &arr[i][0]);
-		printf("podaj priorytet zadania\n");
-		scanf("%llu", &arr[i][1]);
-
-		arr = realloc(arr, (i+2)*sizeof(uint64_t*));
-		arr[i+1] = (uint64_t*)malloc(2*sizeof(uint64_t));
-
-		printf("czas: %llu\t priorytet: %d\n", arr[i][0], arr[i][1]);
-		rt_mutex_acquire(&mutex, TM_INFINITE);
-		ptr[0] = arr[i][0];
-		ptr[1] = arr[i][1];
-		rt_mutex_release(&mutex);
-		i = i + 1;
-	}
-}
 
 int main(int a, char** b){
-	mlockall(MCL_CURRENT | MCL_FUTURE);
-
-	RT_TASK task;
-
-	rt_task_create(&task, "totask", 0, 10, 0);
-	rt_task_start(&task, &toheap, NULL);
-
-	pause();
+	printf("czekam na sterte\n");
+	RT_HEAP heap;
+	rt_heap_bind(&heap, "myheap", TM_INFINITE);
+	printf("czekam na mutex\n");
+	RT_MUTEX mutex;
+	rt_mutex_bind(&mutex, "mymutex", TM_INFINITE);
+	printf("czekam na semafor\n");
+	RT_SEM sem;
+	rt_sem_bind(&sem, "mysem", TM_INFINITE);
+	task_data* ptr;
+	rt_heap_alloc(&heap, 0, TM_INFINITE, (void**)&ptr);
+	while(1){
+		task_data usr_inp;
+		printf("podaj czas trwania: ");
+		scanf("%llu", &usr_inp.time);
+		printf("%llu\n", time);
+		printf("%s\n", &usr_inp.time);
+		printf("podaj priorytet: ");
+		scanf("%d", &usr_inp.prio);
+		rt_mutex_acquire(&mutex, TM_INFINITE);
+		long long ind = ++ptr[0].ind;
+		if(ind == TASK_MAX) {
+			ind = 1;
+			ptr[0].ind = ind;
+		}
+		ptr[ind] = usr_inp;
+		rt_mutex_release(&mutex);
+		rt_sem_v(&sem);
+	}
 }
